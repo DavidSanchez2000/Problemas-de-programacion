f687072c0fa4773013717019ae86f5e6c3fe5939
David Sanchez <davidalejandrosanchezarias058@gmail.com>
2021-07-19 18:40:27 -0500

    Add files via upload

---
+++ brocot.py
@@ -0,0 +1,28 @@
+from sys import stdin
+
+def solve(m,n):
+	res = ""
+	mc, nc = 1, 1
+	ml, nl = 0, 1
+	mr, nr = 1, 0
+
+	while mc != m or nc != n:
+		if m *nc > n* mc:
+			ml, nl = mc, nc
+			res += "R"
+		else:
+			mr, nr = mc, nc
+			res += "L"
+		mc, nc = ml+mr, nl + nr
+	print(res)
+
+
+
+
+def main():
+	while True:
+		m,n = list(map(int, input().split()))
+		if m == n == 1:
+			break
+		solve(m,n)
+main()
